323e9a2cc8bf6c74107afe907c4a7377cca5b0d8
JacobBatson <jacobbatson123@aol.com>
2025-09-23 17:54:01 -0400

    Added movement and 2 practice fighters

---
+++ src/Players/Fighter1.java
@@ -0,0 +1,14 @@
+package Players;
+
+public class Fighter1 extends Player1 {
+
+    // Constructor for fighter
+    public Fighter1(float x, float y) {
+        super(x, y, "Cat.png", 24, 24);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+}
---
+++ src/Players/Fighter2.java
@@ -0,0 +1,13 @@
+package Players;
+
+public class Fighter2 extends Player2 {
+
+    public Fighter2(float x, float y) {
+        super(x, y, "Cat.png", 24, 24);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+}
